8a4b3e481b0b49d764b453331a08b8bc86113770
Grigoriy Abashkin <grigoriy.abashkin@kazzinc.com>
2025-11-10 10:31:51 +0500

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import { Layout } from './features/components/layout/Layout';
 import { AuthForm } from './features/auth/AuthForm';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Layout>
       <div>
-        <AuthForm type="signIn" />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <h1>Продукты</h1>
+              </div>
+            }
+          />
+          <Route path="/registration" element={<AuthForm type="signUp" />} />
+          <Route path="/auth" element={<AuthForm type="signIn" />} />
+          <Route path="*" element={<div>404 Not Found</div>} />
+        </Routes>
       </div>
     </Layout>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import App from './App.tsx';
 import './assets/styles/main.css';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </StrictMode>,
 );
