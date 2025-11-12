dc00bae48b0cd05d7b43c655b441e3a8ace18e58
Grigoriy Abashkin <ab.grigoriy10@gmail.com>
2025-11-12 23:03:08 +0500

    feat: config profile icon and link

--- src/features/components/header/Header.tsx
+++ src/features/components/header/Header.tsx
@@ -1,10 +1,32 @@
 import type { FC } from 'react';
 import styles from './Header.module.css';
+import { UserOutlined } from '@ant-design/icons';
+
+import { useLazyGetProfileQuery } from './../../profile/profileEndpoints';
+import { useSelector } from 'react-redux';
+import type { RootState } from './../../../app/store';
+import { useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
 
 export const Header: FC = () => {
+  const token = useSelector((state: RootState) => state.auth.token);
+  const [trigger, { data: profile }] = useLazyGetProfileQuery();
+
+  useEffect(() => {
+    if (token) trigger();
+  }, [token]);
+
   return (
     <div className={[styles.header].join(' ')}>
-      <h1>Интернет-магазин</h1>
+      <NavLink to="/" className={styles['nav-link']}>
+        <h1>Интернет-магазин</h1>
+      </NavLink>
+      <div style={{ paddingRight: 10 }}>
+        <NavLink to="/profile" className={styles['nav-link']}>
+          <UserOutlined style={{ display: 'block', fontSize: 20 }} />
+          {profile?.name ?? profile?.email}
+        </NavLink>
+      </div>
     </div>
   );
 };
