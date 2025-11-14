8b0d858d447fc3adac1a849a7bf16d37b3a110ea
Grigoriy Abashkin <grigoriy.abashkin@kazzinc.com>
2025-11-14 13:24:26 +0500

    feat: fix size oreder icon

--- src/features/components/buttons/OrdersButton.tsx
+++ src/features/components/buttons/OrdersButton.tsx
@@ -2,13 +2,16 @@ import type { FC } from 'react';
 import { ShoppingOutlined } from '@ant-design/icons';
 import { NavLink } from 'react-router-dom';
 import sytles from './Buttons.module.css';
+import { Badge } from 'antd';
 
 export const OrdersButton: FC = () => {
   return (
     <NavLink to="orders" className={sytles.link}>
-      <ShoppingOutlined
-        style={{ display: 'block', fontSize: '1.9em', color: 'var(--text-color-light)' }}
-      />
+      <Badge count={0} color="var(--color-primary)">
+        <ShoppingOutlined
+          style={{ display: 'block', fontSize: '2em', color: 'var(--text-color-light)' }}
+        />
+      </Badge>
     </NavLink>
   );
 };
