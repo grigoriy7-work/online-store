37070b5e9d9e425d345eff5c092fec0be828c206
Grigoriy Abashkin <work.grigoriy7mail@gmail.com>
2025-11-16 21:40:32 +0500

    Change repository name in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const repoName = 'https://github.com/grigoriy7-work/online-store';
+const repoName = 'online-store';
 
 // https://vite.dev/config/
 export default defineConfig({
