5ca7ffcda1c85681e82e5871510290fc053033ba
Grigoriy Abashkin <ab.grigoriy10@gmail.com>
2025-11-24 21:41:22 +0500

    feat: create scroll products

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -1,26 +1,60 @@
-import { type FC, useEffect, useState } from 'react';
+import { type FC, useCallback, useEffect, useState, useRef } from 'react';
 import styles from './Page.module.css';
 import { ProductList } from '../features/products/ProductList';
 import { useLazyGetProductsQuery } from './../features/products/productEndpoints';
 import { useSelector } from 'react-redux';
 import type { RootState } from './../app/store';
 import { CategoryCardList } from '../features/categories/CategoryCardList';
-import { Spin, Pagination } from 'antd';
+import { Spin } from 'antd';
+import { useScroll } from '../features/components/useScroll';
+import type { Product } from '../app/types/typesProducts';
 
 export const ProductsPage: FC = () => {
   const token = useSelector((state: RootState) => state.auth.token);
-  const [pageNumber, setPageNumber] = useState(1);
   const [trigger, { data, isLoading }] = useLazyGetProductsQuery();
-  const products = data?.data ?? [];
+  let pageNumber = useRef(1);
   const pageSize = 12;
+  const total = data?.pagination?.total ?? 0;
+  const [hasMore, setHasMore] = useState(false);
+  const categoryIdList = useRef<string[]>([]);
 
   useEffect(() => {
-    if (token)
+    setHasMore(pageNumber.current * pageSize < total);
+  }, [pageNumber.current, pageSize, total]);
+
+  const onLoadProducts = () => {
+    if (token) {
       trigger({
+        categoryIds: categoryIdList.current,
         sorting: { field: 'createdAt', type: 'DESC' },
-        pagination: { pageSize: pageSize, pageNumber: pageNumber },
+        pagination: { pageSize: pageSize, pageNumber: pageNumber.current },
       });
-  }, [token, pageNumber]);
+    }
+  };
+
+  const lastElementRef = useScroll(
+    isLoading,
+    hasMore,
+    useCallback(() => {
+      pageNumber.current += 1;
+      onLoadProducts();
+    }, [categoryIdList]),
+  );
+  const [products, setProducts] = useState<Product[]>([]);
+  const dataPage = data?.data ?? [];
+
+  useEffect(() => {
+    if (token) {
+      onLoadProducts();
+    }
+  }, [token]);
+
+  useEffect(() => {
+    setProducts((prev) => {
+      const map = new Map([...prev, ...dataPage].map((item) => [item.id, item]));
+      return Array.from(map.values());
+    });
+  }, [data]);
 
   return (
     <div className={styles.page}>
@@ -29,22 +63,15 @@ export const ProductsPage: FC = () => {
           <div style={{ marginRight: 15 }}>
             <CategoryCardList
               readProducts={(categoryIds) => {
-                trigger({
-                  categoryIds: categoryIds,
-                  sorting: { field: 'createdAt', type: 'DESC' },
-                  pagination: { pageSize: pageSize, pageNumber: pageNumber },
-                });
+                setProducts([]);
+                pageNumber.current = 1;
+                categoryIdList.current = categoryIds;
+                onLoadProducts();
               }}
             />
           </div>
           <div>
-            <ProductList products={products} />
-            <Pagination
-              current={pageNumber}
-              onChange={(page) => setPageNumber(page)}
-              defaultPageSize={pageSize}
-              total={data?.pagination?.total}
-            />
+            <ProductList products={products} lastElementRef={lastElementRef} />
           </div>
         </div>
       </Spin>
